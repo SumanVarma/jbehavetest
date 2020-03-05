d54a916e020d68cbdfdb161263707eee91dffa65
SumanVarma <sumanvarma83@gmail.com>
2020-03-05 21:52:21 +0530

    Initial Commit

---
+++ src/test/java/GithubUserResponseMediaTypeStoryLiveTest.java
@@ -0,0 +1,13 @@
+public class GithubUserResponseMediaTypeStoryLiveTest extends AbstractStory {
+
+    @Override
+    String storyName() {
+        return "github_user_response_mediatype.story";
+    }
+
+    @Override
+    Object stepInstance() {
+        return new GithubUserResponseMediaTypeSteps();
+    }
+
+}
